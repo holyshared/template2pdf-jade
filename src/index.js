6205706fdaf65b1a8ee9b3592bf6c2fdd8bde1b6
holyshared <holy.shared.design@gmail.com>
2015-09-20 21:49:40 +0900

    add flowtype

--- src/index.js
+++ src/index.js
@@ -1,16 +1,25 @@
+/* @flow */
+
 import _ from 'lodash';
 import jade from 'jade';
 import Promise from 'bluebird';
 
-export default function renderer(options = {}) {
+type JadeOptions = {
+  filename?: string,
+  compileDebug?: boolean,
+  pretty?: boolean
+};
+
+export default function renderer(options: JadeOptions = {}): JadeRenderer {
   return new JadeRenderer(options);
 }
 
 export class JadeRenderer {
-  constructor(options = {}) {
+  options: JadeOptions;
+  constructor(options: JadeOptions = {}) {
     this.options = options;
   }
-  render(template, values = {}) {
+  render(template: string, values: Object = {}) : Promise {
     return new Promise((resolve, reject) => {
       let locals = _.merge(this.options, values);
       try {
